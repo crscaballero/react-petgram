0c0dd18f720b07ad2f45ac0f40b5a63575d0c156
Cristian Caballero <crsecaballero@gmail.com>
2019-08-17 21:59:27 -0700

    Protected Routes
    https://platzi.com/clases/1601-react-avanzado/21282-rutas-protegid-1/

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Router } from '@reach/router';
-
 import { GlobalStyle } from './styles/GlobalStyles';
-
+import { Logo } from './components/Logo';
 import { NavBar } from './components/NavBar';
 
-import { Logo } from './components/Logo';
 import { Home } from './pages/Home';
 import { Detail } from './pages/Detail';
+import { Favs } from './pages/Favs';
+import { User } from './pages/User';
+import { NotRegisteredUser } from './pages/NotRegisteredUser';
+
+const UserLogged = ({ children }) => {
+    return children({ isAuth: true });
+};
 
 export const App = () => {
     return (
@@ -16,9 +21,26 @@ export const App = () => {
             <Logo />
             <Router>
                 <Home path='/' />
-                <Home path='/pet/:id' />
+                <Home path='/pet/:categoryId' />
                 <Detail path='/detail/:detailId' />
             </Router>
+
+            <UserLogged>
+                {({ isAuth }) =>
+                    isAuth ? (
+                        <Router>
+                            <Favs path='/favs' />
+                            <User path='/user' />
+                        </Router>
+                    ) : (
+                        <Router>
+                            <NotRegisteredUser path='/favs' />
+                            <NotRegisteredUser path='/user' />
+                        </Router>
+                    )
+                }
+            </UserLogged>
+
             <NavBar />
         </div>
     );
