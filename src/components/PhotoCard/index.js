e6057147694c79a93c2e97273f60eb5c88e95d9b
Cristian Caballero <crsecaballero@gmail.com>
2019-08-17 21:30:12 -0700

    Fix route in Detaill Page
    https://platzi.com/clases/1601-react-avanzado/21279-creando-la-pagina-detal-6/

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-//import { Link } from '@reach/router';
+import { Link } from '@reach/router';
 
 import { Article, ImgWrapper, Img } from './styles';
 import { useLocalStorage } from '../../hooks/useLocalStorage';
@@ -19,11 +19,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
         <Article ref={element}>
             {show && (
                 <Fragment>
-                    <a href={`/?detail=${id}`}>
+                    <Link to={`/detail/${id}`}>
                         <ImgWrapper>
                             <Img src={src} />
                         </ImgWrapper>
-                    </a>
+                    </Link>
                     <ToggleLikeMutation>
                         {toggleLike => {
                             const handleFavClick = () => {
